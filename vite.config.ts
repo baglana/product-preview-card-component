053bd0ab9441cd143d20935a5549cfb86b583538
baglana <102351104+baglana@users.noreply.github.com>
2024-01-21 20:29:00 +0600

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+/** @type {import("vite").UserConfig} */
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: "https://baglana.github.io/product-preview-card-component",
+});
