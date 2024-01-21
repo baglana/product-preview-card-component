91ba88b2489dc163f20a200caf2f5a97e490a11c
baglana <102351104+baglana@users.noreply.github.com>
2024-01-21 19:18:00 +0600

    Fix main.ts script import and divide css into parts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,7 @@
-import './modern-css-reset';
+import 'modern-css-reset';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
+import '@fontsource/fraunces/700.css';
+import './button.css';
+import './card.css';
 import './style.css';
